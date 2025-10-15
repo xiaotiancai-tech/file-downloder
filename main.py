d6321e4b7f1eee1d6f3c01f82e31d24e8f22f63e
xiaotiancaipro <xiaotiancaipro@163.com>
2025-10-15 11:28:22 +0800

    Feat: 配置数据路径

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ class ThreadedHTTPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
 
 
 class CustomHandler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory="data", **kwargs)
+
     def log_message(self, format, *args):
         thread_name = threading.current_thread().name
         print(f"[{thread_name}] {format % args}")
