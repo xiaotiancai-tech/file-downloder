f6eb78beaf307a352734a672c6619c55b0479d0d
xiaotiancaipro <xiaotiancaipro@163.com>
2025-10-15 11:15:16 +0800

    Feat: 实现文件下载服务

---
+++ main.py
@@ -0,0 +1,23 @@
+import http.server
+import socketserver
+import threading
+
+
+class ThreadedHTTPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    daemon_threads = True
+
+
+class CustomHandler(http.server.SimpleHTTPRequestHandler):
+    def log_message(self, format, *args):
+        thread_name = threading.current_thread().name
+        print(f"[{thread_name}] {format % args}")
+
+
+if __name__ == "__main__":
+    PORT = 8000
+    with ThreadedHTTPServer(("", PORT), CustomHandler) as httpd:
+        print(f"服务启动成功, 运行端口为 {PORT}")
+        try:
+            httpd.serve_forever()
+        except KeyboardInterrupt:
+            print("\n服务已停止")
